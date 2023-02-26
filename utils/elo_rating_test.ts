e9042b4f1856d8a3b84ccc612924ba35a4fcbc68
radjavi <radjavi@hotmail.com>
2023-02-26 20:28:31 +0100

    fix: use 0 score in elo test

--- utils/elo_rating_test.ts
+++ utils/elo_rating_test.ts
@@ -9,8 +9,8 @@ Deno.test("Should calculate elo rating for two equally rated players", () => {
 });
 
 Deno.test("Should calculate elo rating for two unequally rated players", () => {
-  const [R_A2, R_B2] = calculateRating(984, 1016, 1);
+  const [R_A2, R_B2] = calculateRating(1016, 984, 0);
 
-  assertEquals(R_A2.toFixed(1), "1001.5");
-  assertEquals(R_B2.toFixed(1), "998.5");
+  assertEquals(R_A2.toFixed(1), "998.5");
+  assertEquals(R_B2.toFixed(1), "1001.5");
 });
